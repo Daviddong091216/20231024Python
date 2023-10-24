8e4b5e6b7817ca32396a3a688711339aaf91dd4c
David Dong <daviddong201612@gmail.com>
2023-10-24 17:02:32 -0500

    terminated an AWS EC2 instance by Python scripting

--- AWS_Scripting.py
+++ AWS_Scripting.py
@@ -1,6 +1,7 @@
 import boto3
 ec2=boto3.resource('ec2')
 
+'''
 instance=ec2.create_instances(
     ImageId='ami-25615740',
     MinCount=1,
@@ -8,3 +9,8 @@ instance=ec2.create_instances(
     InstanceType='t2.micro'
 )
 print(instance[0].id)
+'''
+instance_id='i-07c85a80807fd90c4'
+instance=ec2.Instance(instance_id)
+response=instance.terminate()
+print(response)
